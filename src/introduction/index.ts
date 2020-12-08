2ec0afccd851be03f54e3e6b31e330f4222ca2e8
on3dd <onedeadwave@gmail.com>
2020-12-08 16:37:25 +1000

    formatting

--- src/introduction/index.ts
+++ src/introduction/index.ts
@@ -1,52 +1,52 @@
 type InterpreterState = {
-	value: number;
-	output: string;
+  value: number;
+  output: string;
 };
 
 type Commands = {
-	'.': '.';
-	'+': '+';
+  '.': '.';
+  '+': '+';
 };
 
 type Command = keyof Commands;
 
 const COMMANDS: Commands = {
-	'+': '+',
-	'.': '.',
+  '+': '+',
+  '.': '.',
 };
 
 export const myFirstInterpreter = (code: string): string => {
-	const DEFAULT_STATE: InterpreterState = {
-		value: 0,
-		output: '',
-	};
+  const DEFAULT_STATE: InterpreterState = {
+    value: 0,
+    output: '',
+  };
 
-	const add = (state: InterpreterState): InterpreterState => {
-		const value = (state.value + 1) % 256;
-		return { ...state, value };
-	};
+  const add = (state: InterpreterState): InterpreterState => {
+    const value = (state.value + 1) % 256;
+    return { ...state, value };
+  };
 
-	const print = (state: InterpreterState): InterpreterState => {
-		const output = state.output + String.fromCharCode(state.value);
-		return { ...state, output };
-	};
+  const print = (state: InterpreterState): InterpreterState => {
+    const output = state.output + String.fromCharCode(state.value);
+    return { ...state, output };
+  };
 
-	const interprete = (char: string, state: InterpreterState) => {
-		switch (char as Command) {
-			case COMMANDS['+']:
-				return add(state);
+  const interprete = (char: string, state: InterpreterState) => {
+    switch (char as Command) {
+      case COMMANDS['+']:
+        return add(state);
 
-			case COMMANDS['.']:
-				return print(state);
+      case COMMANDS['.']:
+        return print(state);
 
-			default:
-				return state;
-		}
-	};
+      default:
+        return state;
+    }
+  };
 
-	const result = code.split('').reduce((acc, curr) => {
-		return interprete(curr, acc);
-	}, DEFAULT_STATE);
+  const result = code.split('').reduce((acc, curr) => {
+    return interprete(curr, acc);
+  }, DEFAULT_STATE);
 
-	return result.output;
+  return result.output;
 };
